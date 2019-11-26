d60f28da0c072d81c96a916b0c69e1d9ddbd7b04
manmanandman <prakasit_sw2@outlook.com>
2019-11-26 11:46:58 +0700

    sd

--- home.php
+++ home.php
@@ -27,17 +27,41 @@
         <div class="homedetail">
             <img src="./pic/boy under tree.png" style="width: 500px;float: right;margin: auto;">
             <br><br><br><br><br>
-            <span class="text1">Let's create the</span>
-            <span class="text2">campaign</span>
-            <br>
-            <span class="text2">that make</span>
-            <span class="text3">our world better</span><br><br>
+            <div class="textmain"style="display: block;" >
+                <span class="text1">Let's create the</span>
+                <span class="text2">campaign</span>
+                <br>
+                <span class="text2">that make</span>
+                <span class="text3">our world better</span><br><br>
+            </div>
+            <div class="textjoin" style="display: none;" >
+                <span class="text1">Join</span>
+                <span class="text2">campaign</span>
+                <br>
+                <span class="text2">that you</span>
+                <span class="text3">interested</span><br><br>
+            </div>
+            <div class="textrate" style="display: none;">
+                <span class="text1">Rate the</span>
+                <span class="text2">campaign</span>
+                <br>
+                <span class="text2">to make </span>
+                <span class="text3">better one</span><br><br>
+            </div>
+            <div class="textview" style="display: none;">
+                <span class="text1">Find interesting</span>
+                <span class="text2">campaign</span>
+                <br>
+                <span class="text2">in many</span>
+                <span class="text3">category</span><br><br>
+            </div>
             <a href="profile.php"><button class="btn2">Get Start Now</button></a>
             <div class="extradetail">
-                <img class="picdetail" src="./pic/home-create.png">
-                <img class="picdetail" src="./pic/home-join.png">
-                <img class="picdetail" src="./pic/home-rate.png">
-                <img class="picdetail" src="./pic/home-view.png">
+                <img class="picdetail" id="piccreate" src="./pic/home-create.png">
+                <img class="picdetail" id="picjoin" src="./pic/home-join.png">
+                <img class="picdetail" id="picrate" src="./pic/home-rate.png">
+                <img class="picdetail" id="picview" src="./pic/home-view.png">
+
             </div>
         </div>
     </div>

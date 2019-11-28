95573f45288c36305c621780ffa51a54c8bc1141
manmanandman <prakasit_sw2@outlook.com>
2019-11-28 16:43:29 +0700

    a

--- adminview.php
+++ adminview.php
@@ -10,13 +10,13 @@
     <link href="./css/style.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
     <link href="./css/view1.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
     <?php
-        require 'connect.php';
-        $sql = "SELECT * FROM campaigninfo WHERE status = 1";
-        $sqlcount = "SELECT COUNT(*) num FROM campaigninfo WHERE status = 1";
-        $result = mysqli_query($con, $sqlcount);
-        $row = mysqli_fetch_array($result);
-        $num = $row['num'];
-        $result = mysqli_query($con, $sql);
+    require 'connect.php';
+    $sql = "SELECT * FROM campaigninfo WHERE campaign_id = " . $_POST['cid'];
+    $result = mysqli_query($con, $sql);
+    $row = mysqli_fetch_array($result);
+    $sqlname = "SELECT * FROM userinfo WHERE user_id = " . $row['user_id'];
+    $result2 = mysqli_query($con, $sqlname);
+    $user = mysqli_fetch_array($result2);
     ?>
 </head>
 
@@ -26,33 +26,34 @@
             <a><img style="width: 135px" src="./pic/icon.png"></a>
         </div>
         <div class="nav-right">
-        <span id="text"><a href="./profileadmin.php">Main menu</a></span>
+            <span id="text"><a href="./profileadmin.php">Main menu</a></span>
             <a href="./signout.php"><button class="btn1">Sign out</button></a>
         </div>
     </div>
 
     <div class="contain">
-    <?php
-        for($x=0;$x<$num;$x++){
+        <?php
+        for ($x = 0; $x < $num; $x++) {
             $row = mysqli_fetch_array($result);
             echo '<div class="boxview">
-                <img class="pic" id="pic" src="'.$row['campaign_pic'].'"><br>
-                <span class="text-campaignname" id="camname">'.$row['campaign_name'].'</span>
+                <img class="pic" id="pic" src="' . $row['campaign_pic'] . '"><br>
+                <span class="text-campaignname" id="camname">' . $row['campaign_name'] . '</span>
                 <div class="campaigndetailtextbox">
                     <div class="nav-left2 ">
                         <img class="picicon" id="picDate" src="./pic/calendar.png"></img>
-                        <span class="text-campaignsub" id="camdate">Date : '.$row['start_time'].'</span>
+                        <span class="text-campaignsub" id="camdate">Date : ' . $row['start_time'] . '</span>
                         <br>
                         <img class="picicon" id="picLocation" src="./pic/location.png"></img>
-                        <span class="text-campaignsub" id="camlocation">Location : '.$row['location'].'</span>
+                        <span class="text-campaignsub" id="camlocation">Location : ' . $row['location'] . '</span>
                         <br>
                         <img class="picicon" id="picSize" src="./pic/people.png"></img>
-                        <span class="text-campaignsub" id="camsize">Size : '.$row['amount_people'].'</span><br>
+                        <span class="text-campaignsub" id="camsize">Size : ' . $row['amount_people'] . '</span><br>
                     </div>
                 </div>
                 <div class="campaigndetailbutton">
-                    <button type="submit" name="uid" form="select-form" value="'.$row['campaign_id'].'" class="btn2">Read More</button></a>
-
+                    <form action="./adminviewdetail.php" method="post" id="select-form">
+                        <button type="submit" name="cid" form="select-form" value="' . $row['campaign_id'] . '" class="btn2">Read More</button>
+                    </form>
                 </div>
             </div>';
         }

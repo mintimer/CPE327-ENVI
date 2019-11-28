6479392214ae6489682e0d5510731bb4fcc967b7
manmanandman <prakasit_sw2@outlook.com>
2019-11-29 01:16:26 +0700

    update database

--- envi.sql
+++ envi.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2019 at 05:18 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.1
+-- Generation Time: Nov 28, 2019 at 07:15 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,16 +36,16 @@ CREATE TABLE `campaigninfo` (
   `end_time` date NOT NULL,
   `campaign_describe` text COLLATE utf8_unicode_ci NOT NULL,
   `manage_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `manage_authen` text COLLATE utf8_unicode_ci,
-  `campaign_document` text COLLATE utf8_unicode_ci,
+  `manage_authen` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `campaign_document` text COLLATE utf8_unicode_ci DEFAULT NULL,
   `campaign_pic` text COLLATE utf8_unicode_ci NOT NULL,
   `amount_people` int(11) NOT NULL,
   `location` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `status` tinyint(4) NOT NULL DEFAULT 0,
   `user_id` int(11) NOT NULL,
   `lati` double NOT NULL,
   `longti` double NOT NULL,
-  `rating_avg` float NOT NULL DEFAULT '0'
+  `rating_avg` float NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -55,7 +55,9 @@ CREATE TABLE `campaigninfo` (
 INSERT INTO `campaigninfo` (`campaign_id`, `campaign_name`, `campaign_type`, `start_time`, `end_time`, `campaign_describe`, `manage_name`, `manage_authen`, `campaign_document`, `campaign_pic`, `amount_people`, `location`, `status`, `user_id`, `lati`, `longti`, `rating_avg`) VALUES
 (1, 'Push da payload', 3, '2019-12-30', '2019-12-31', 'Lets help DVA to got this win of payload pushing now !!!!!', 'D.Va', NULL, NULL, './pic/campaign/Push da payload.png', 12, 'Busan', 1, 9, 35.14876876243253, 129.0537048847939, 0),
 (2, 'Plook Prayad', 1, '2019-12-28', '2019-12-31', 'Plook kun ter rao sao pai tum mai ya mua arai wa eiei zaza!!', 'Harn Bangmod', NULL, NULL, './pic/campaign/Plook Prayad.jpg', 44, 'Bang mod', 1, 3, 13.651032936218165, 100.49537658691406, 0),
-(6, 'Lang Par Char', 2, '2020-01-15', '2020-01-22', 'A Stop on the Salt Route 1000 B.C. As they rounded a bend in the path that ran beside the river, Lara recognized the silhouette of a fig tree atop a nearby hill. The weather was hot and the days were long. The fig tree was in full leaf, but not yet bearing fruit. Soon Lara spotted other landmarksâ€”an outcropping of limestone beside the path that had a silhouette like a manâ€™s face, a marshy spot beside the river where the waterfowl were easily startled, a tall tree that looked like a man with his arms upraised. They were drawing near to the place where there was an island in the river. The island was a good spot to make camp. They would sleep on the island tonight. Lara had been back and forth along the river path many times in her short life. Her people had not created the pathâ€”it had always been there, like the riverâ€”but their deerskin-shod feet and the wooden wheels of their handcarts kept the path well worn. Laraâ€™s people were salt traders, and their livelihood took them on a continual journey.', 'Polar Bear', NULL, NULL, './pic/campaign/Lang Par Char.jpg', 10, 'Greenland', 0, 3, 60.05032975791311, -43.19330771519503, 0);
+(6, 'Lang Par Char', 2, '2020-01-15', '2020-01-22', 'A Stop on the Salt Route 1000 B.C. As they rounded a bend in the path that ran beside the river, Lara recognized the silhouette of a fig tree atop a nearby hill. The weather was hot and the days were long. The fig tree was in full leaf, but not yet bearing fruit. Soon Lara spotted other landmarksâ€”an outcropping of limestone beside the path that had a silhouette like a manâ€™s face, a marshy spot beside the river where the waterfowl were easily startled, a tall tree that looked like a man with his arms upraised. They were drawing near to the place where there was an island in the river. The island was a good spot to make camp. They would sleep on the island tonight. Lara had been back and forth along the river path many times in her short life. Her people had not created the pathâ€”it had always been there, like the riverâ€”but their deerskin-shod feet and the wooden wheels of their handcarts kept the path well worn. Laraâ€™s people were salt traders, and their livelihood took them on a continual journey.', 'Polar Bear', NULL, NULL, './pic/campaign/Lang Par Char.jpg', 10, 'Greenland', 0, 3, 60.05032975791311, -43.19330771519503, 0),
+(7, 'The World', 4, '2020-03-05', '2020-04-24', 'KILL THANOS', 'TONY STARK', NULL, NULL, './pic/campaign/The World.jpg', 99, 'BENGBENG', 1, 9, 13.637535802375337, 87.23495707392317, 0),
+(8, 'Clean Egypt', 1, '2020-05-01', '2020-05-02', 'MUDAMUDAMUMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDADAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDAMUDA', 'DIO', NULL, NULL, './pic/campaign/Clean Egypt.jpg', 10, 'Nile River', 0, 11, 26.98527590307631, 31.378434396274997, 0);
 
 -- --------------------------------------------------------
 
@@ -73,10 +75,10 @@ CREATE TABLE `userinfo` (
   `phone_no` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
   `dob` date NOT NULL,
-  `disease` text COLLATE utf8_unicode_ci,
-  `allergic_food` text COLLATE utf8_unicode_ci,
-  `banned` int(11) NOT NULL DEFAULT '0',
-  `picture_path` text COLLATE utf8_unicode_ci
+  `disease` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `allergic_food` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `banned` int(11) NOT NULL DEFAULT 0,
+  `picture_path` text COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -87,7 +89,9 @@ INSERT INTO `userinfo` (`user_id`, `username`, `password`, `firstname`, `lastnam
 (1, 'admin', '1234', 'Prayad', 'Janjao', 9999, '0851957830', 'admin@envi.org', '1998-08-21', NULL, NULL, 0, './pic/profile/adminpic.jpg'),
 (3, 'mint', '1234', 'Settapong', 'Subkong', 100, '0851957830', 'mint@envi.org', '1998-08-21', NULL, NULL, 0, './pic/profile/mint.jpg'),
 (5, 'test', '1150', 'Test', 'Unknown', 100, '191', 'test@test.test', '1982-06-11', NULL, NULL, 0, ''),
-(9, 'man', '1234', 'Prakasit', 'Nuchkamnerd', 100, '191', 'man@hotmail.com', '1998-09-20', NULL, NULL, 0, './pic/profile/man.jpg');
+(9, 'man', '1234', 'Prakasit', 'Nuchkamnerd', 100, '191', 'man@hotmail.com', '1998-09-20', NULL, NULL, 0, './pic/profile/man.jpg'),
+(10, 'k', '1234', 'Kakyoin', 'Noriaki', 100, '00', 'k@hotmail.com', '2008-11-20', NULL, NULL, 0, './pic/profile/k.jpg'),
+(11, 'd', '1234', 'DIO', 'DEJANERO', 100, '00000', 'd@hotmail.com', '1978-01-16', NULL, NULL, 0, './pic/profile/d.jpg');
 
 -- --------------------------------------------------------
 
@@ -99,7 +103,7 @@ CREATE TABLE `user_join` (
   `user_id` int(11) NOT NULL,
   `campaign_id` int(11) NOT NULL,
   `rating_score` int(11) DEFAULT NULL,
-  `comment` text COLLATE utf8_unicode_ci
+  `comment` text COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -110,7 +114,16 @@ INSERT INTO `user_join` (`user_id`, `campaign_id`, `rating_score`, `comment`) VA
 (3, 2, NULL, NULL),
 (3, 6, NULL, NULL),
 (5, 1, NULL, NULL),
-(9, 1, NULL, NULL);
+(5, 7, NULL, NULL),
+(9, 1, NULL, NULL),
+(9, 7, NULL, NULL),
+(10, 1, NULL, NULL),
+(10, 2, NULL, NULL),
+(10, 7, NULL, NULL),
+(11, 1, NULL, NULL),
+(11, 2, NULL, NULL),
+(11, 7, NULL, NULL),
+(11, 8, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -145,13 +158,13 @@ ALTER TABLE `user_join`
 -- AUTO_INCREMENT for table `campaigninfo`
 --
 ALTER TABLE `campaigninfo`
-  MODIFY `campaign_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `campaign_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `userinfo`
 --
 ALTER TABLE `userinfo`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Constraints for dumped tables
